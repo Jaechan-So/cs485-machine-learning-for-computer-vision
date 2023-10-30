4ccacf7c6319145e259b756fca0fb6112c0b863c
Jaechan-So <jaechan331@gmail.com>
2023-10-30 14:28:43 +0900

    apply protected access modifier

--- coursework-1/src/eigen_face.py
+++ coursework-1/src/eigen_face.py
@@ -11,27 +11,27 @@ def pca(matrix, data_count):
 
 class EigenFace:
     def __init__(self, face_data: FaceData):
-        self.eigen_vector_n = None
-        self.eigen_value_n = None
-        self.face_data = face_data
+        self._eigen_vector = None
+        self._eigen_value = None
+        self._face_data = face_data
 
-    def compute_eig_d(self):
-        return pca(self.face_data.feature_train, self.face_data.data_count)
+    def _compute_eig_d(self):
+        return pca(self._face_data.feature_train, self._face_data.data_count)
 
-    def compute_eig_n(self):
-        return pca(np.transpose(self.face_data.feature_train), self.face_data.data_count)
+    def _compute_eig_n(self):
+        return pca(np.transpose(self._face_data.feature_train), self._face_data.data_count)
 
     @measure_time(measure_time_tag='Q1-a')
-    def compute_eig_d_with_measure_time(self):
-        return self.compute_eig_d()
+    def _compute_eig_d_with_measure_time(self):
+        return self._compute_eig_d()
 
     @measure_time(measure_time_tag='Q1-b')
-    def compute_eig_n_with_measure_time(self):
-        return self.compute_eig_n()
+    def _compute_eig_n_with_measure_time(self):
+        return self._compute_eig_n()
 
     def test_dimensionality(self):
-        eigen_value_d, _ = self.compute_eig_d_with_measure_time()
-        eigen_value_n, _ = self.compute_eig_n_with_measure_time()
+        eigen_value_d, _ = self._compute_eig_d_with_measure_time()
+        eigen_value_n, _ = self._compute_eig_n_with_measure_time()
         print('Therefore, using (1/N)(A^T)A is more efficient with respect to time and space.')
         print(
             f'# of eigenvalues using (1/N)(A^T)A: {len(eigen_value_n)}, # of eigenvalues which are also in the set of eigenvalues using (1/N)A(A^T): {len([x in set(eigen_value_d) for x in eigen_value_n])}'
