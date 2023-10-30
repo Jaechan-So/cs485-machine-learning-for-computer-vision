4f758cdbc02a7d7bafe0a91f6b1205dc5b55f6e6
Jaechan-So <jaechan331@gmail.com>
2023-10-30 20:14:02 +0900

    add eigen_face related tests

--- coursework-1/src/main.py
+++ coursework-1/src/main.py
@@ -5,9 +5,13 @@ from face_data import FaceData
 def main():
     face_data = FaceData()
 
-    # Q1
+    # Q1 - Eigenfaces
     eigen_face = EigenFace(face_data)
     eigen_face.test_dimensionality()
+
+    # Q1 - Application of Eigenfaces
+    eigen_face.preprocess_eigen()
+    eigen_face.test_reconstruction()
 
 
 if __name__ == '__main__':
