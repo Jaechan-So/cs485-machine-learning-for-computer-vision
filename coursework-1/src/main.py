941cb48d70a91257a9f08984b250d295c42f6052
Jaechan-So <jaechan331@gmail.com>
2023-11-03 00:41:57 +0900

    change usage according to changed classes

--- coursework-1/src/main.py
+++ coursework-1/src/main.py
@@ -1,17 +1,18 @@
 from eigen_face import EigenFace
 from face_data import FaceData
+from face_reconstruction import FaceReconstruction
 
 
 def main():
     face_data = FaceData()
 
     # Q1 - Eigenfaces
-    eigen_face = EigenFace(face_data)
-    eigen_face.test_dimensionality()
+    eigen_face = EigenFace(face_data=face_data)
+    # eigen_face.test_dimensionality()
 
     # Q1 - Application of Eigenfaces
-    eigen_face.preprocess_eigen()
-    eigen_face.test_reconstruction()
+    face_reconstruction = FaceReconstruction(face_data=face_data)
+    face_reconstruction.test_reconstruction()
 
 
 if __name__ == '__main__':
