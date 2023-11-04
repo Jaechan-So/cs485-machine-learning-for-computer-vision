a93ecfe0bd9eb0bc2251da137a1dd4cd824b9d33
Jaechan-So <jaechan331@gmail.com>
2023-11-04 21:41:41 +0900

    refactor NN face recognition codes

--- coursework-1/src/nearest_neighbor_face_recognition.py
+++ coursework-1/src/nearest_neighbor_face_recognition.py
@@ -32,16 +32,19 @@ class NearestNeighborFaceRecognition:
         return self._face_data.label_train.T.reshape(-1)[nearest_neighbor_index]
 
     @measure_time(tag='Q1 - Application of Eigenfaces - b: Computing Nearest Neighbors')
-    def _compute_nearest_neighbors(self, eigen_vectors, norm):
+    def _compute_nearest_neighbors_predictions(self, eigen_vectors, norm):
         return np.array([self._find_nearest_neighbor(eigen_vectors, norm, face) for face in
                          self._face_data.feature_test.T])
 
+    @separate_logs
     def _compute_nearest_neighbors_and_evaluate_result(self, num_of_eigen, norm_name, norm):
+        print(f'Compute nearest neighbors and evaluate results with # of eigenvectors: {num_of_eigen}, {norm_name}')
         eigen_vectors = self._face_data.eigen_vector[:num_of_eigen].T
-        predictions = self._compute_nearest_neighbors(eigen_vectors, norm)
+        predictions = self._compute_nearest_neighbors_predictions(eigen_vectors, norm)
         labels = self._face_data.label_test.reshape(-1)
 
-        self._evaluate_result(predictions, labels, num_of_eigen, norm_name)
+        total_count = self._face_data.feature_test.T.shape[0]
+        evaluate_face_recognition_result(total_count, predictions, labels)
 
     def test_nearest_neighbor_recognition(self):
         for (num_of_eigen, norm_name, norm) in context['test_parameters']:
