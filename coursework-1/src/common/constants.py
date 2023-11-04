d414e63537b7619f6979de2d26f4955245a92ef9
Jaechan-So <jaechan331@gmail.com>
2023-11-04 12:58:11 +0900

    add eigen_counts, norms and test_parameters in context

--- coursework-1/src/common/constants.py
+++ coursework-1/src/common/constants.py
@@ -1,6 +1,21 @@
+import itertools
+
+import numpy as np
+
+eigen_counts = [3, 10, 50, 100, 200]
+norms = {
+    'L1 Norm': lambda x1, x2: np.linalg.norm(x1 - x2, ord=1),
+    'L2 Norm': lambda x1, x2: np.linalg.norm(x1 - x2, ord=2),
+}
+test_parameters = [(eigen_count, name, norms[name]) for eigen_count, name in
+                   list(itertools.product(eigen_counts, norms))]
+
 context = {
     'face_row': 46,
     'face_column': 56,
+    'eigen_counts': eigen_counts,
+    'norms': norms,
+    'test_parameters': test_parameters,
 }
 
 config = {
