68e0738d382074b3ae6120cf3641c163c79c82fa
Jaechan-So <jaechan331@gmail.com>
2023-11-04 21:42:12 +0900

    rearrange main flow code

--- coursework-1/src/main.py
+++ coursework-1/src/main.py
@@ -1,6 +1,10 @@
+import matplotlib.pyplot as plt
+
 from eigen_face import EigenFace
+from eigen_space_class_face_recognition import EigenSpaceClassFaceRecognition
 from face_data import FaceData
 from face_reconstruction import FaceReconstruction
+from nearest_neighbor_face_recognition import NearestNeighborFaceRecognition
 
 
 def main():
@@ -12,7 +16,15 @@ def main():
 
     # Q1 - Application of Eigenfaces
     face_reconstruction = FaceReconstruction(face_data=face_data)
-    face_reconstruction.test_reconstruction()
+    # face_reconstruction.test_reconstruction()
+
+    nearest_neighbor_face_recognition = NearestNeighborFaceRecognition(face_data=face_data)
+    # nearest_neighbor_face_recognition.test_nearest_neighbor_recognition()
+
+    eigen_space_class_face_recognition = EigenSpaceClassFaceRecognition(face_data=face_data)
+    eigen_space_class_face_recognition.test_eigen_space_class_face_recognition()
+
+    plt.show()
 
 
 if __name__ == '__main__':
