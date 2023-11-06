65a91845a359b401916f0075232e63b644bcac67
Jaechan-So <jaechan331@gmail.com>
2023-11-06 22:59:22 +0900

    adjust face reconstruction

--- coursework-1/src/face_reconstruction.py
+++ coursework-1/src/face_reconstruction.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from common.constants import context
+from common.constants import norms
+from common.operations import get_reconstructions, reshape_face_for_plot
 from face_data import FaceData
 
 choice_count = 3
@@ -16,21 +17,8 @@ class FaceReconstruction:
         choice_indicies = np.random.choice(faces.shape[0], size=choice_count, replace=False)
         return faces[choice_indicies, :]
 
-    def _reconstruct_face(self, face, num_of_eigen):
-        reconstructed_face = np.copy(self._face_data.mean_face)
-
-        for i in range(num_of_eigen):
-            target_eigen_vector = self._face_data.eigen_vectors[i]
-            a = (face - self._face_data.mean_face) @ target_eigen_vector
-            reconstructed_face += a * target_eigen_vector
-
-        return reconstructed_face
-
     def _reconstruct_faces(self, faces, num_of_eigen):
-        return np.array([self._reconstruct_face(face, num_of_eigen) for face in faces])
-
-    def _reshape_face_for_plot(self, face):
-        return face.reshape(context['face_row'], context['face_column']).T
+        return get_reconstructions(faces, self._face_data.mean_face, self._face_data.eigen_vectors[:num_of_eigen])
 
     def _reconstruct_and_display_faces(self, faces, title):
         sample_faces = self._get_sample_faces(faces)
@@ -43,22 +31,31 @@ class FaceReconstruction:
         fig, ax = plt.subplots(len(eigen_counts) + 1, choice_count)
         fig.suptitle(title, fontsize=16)
 
-        row_titles = ['Original'] + [f'Reconstructed with {count} best eigenvectors' for count in eigen_counts]
+        row_titles = ['Original'] + [f'M = {count}' for count in eigen_counts]
 
         for r, ax_row in enumerate(ax):
             for c, ax_col in enumerate(ax_row):
-                ax_col.imshow(self._reshape_face_for_plot(all_faces[r][c]), cmap='gist_gray')
+                ax_col.imshow(reshape_face_for_plot(all_faces[r][c]), cmap='gist_gray')
                 ax_col.axis('off')
 
         for r, row_title in enumerate(row_titles):
             side = 1 / len(row_titles)
-            fig.text(0.02, 1 - (r + 0.5) * side, row_title, fontsize=12)
+            fig.text(0.02, (1 - (r + 0.5) * side) * 0.85, row_title, fontsize=12)
 
         plt.tight_layout(rect=(0, 0, 0.9, 0.95))
+
+        face_differences = sample_faces - reconstructed_faces
+        for eigen_count, diff in zip(eigen_counts, face_differences):
+            theoretical_reconstruction_error = np.sum(self._face_data.eigen_values[eigen_count:])
+
+            face_count = diff.shape[0]
+            experimental_reconstruction_error = np.sum(np.array([norms['L2 Norm'](d) for d in diff])) / face_count
+            print(
+                f'{title} with M = {eigen_count}: theoretical - {theoretical_reconstruction_error}, experimental: {experimental_reconstruction_error}')
 
     def test_reconstruction(self):
         faces_train = self._face_data.feature_train.T
         faces_test = self._face_data.feature_test.T
 
-        self._reconstruct_and_display_faces(faces_train, 'Train face image samples')
-        self._reconstruct_and_display_faces(faces_test, 'Test face image samples')
+        self._reconstruct_and_display_faces(faces_train, 'Train face image reconstruction')
+        self._reconstruct_and_display_faces(faces_test, 'Test face image reconstruction')
