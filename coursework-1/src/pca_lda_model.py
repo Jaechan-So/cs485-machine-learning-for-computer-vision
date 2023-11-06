64173704747a9b4a20737fb8a0cba5b2b5e7af66
Jaechan-So <jaechan331@gmail.com>
2023-11-06 23:00:07 +0900

    adjust PCA-LDA model

--- coursework-1/src/pca_lda_model.py
+++ coursework-1/src/pca_lda_model.py
@@ -10,16 +10,16 @@ class PCALDAModel:
         self._m_pca = m_pca
         self._m_lda = m_lda
 
-        pca_projections_train = get_eigen_projections(features_train, mean_face, m_pca, eigen_vectors)
+        pca_projections_train = get_eigen_projections(features_train, mean_face, eigen_vectors[:m_pca])
         self._pca_projections_mean_train = np.mean(pca_projections_train, axis=0)
-        lda_eigen_values_train, self._lda_eigen_vectors_train = get_lda_eigen(pca_projections_train, labels_train)
+        lda_eigen_values_train, self._lda_eigen_vectors_train = get_lda_eigen(features_train, labels_train,
+                                                                              m_pca)
         self._lda_projections_train = get_eigen_projections(pca_projections_train, self._pca_projections_mean_train,
-                                                            m_lda,
-                                                            self._lda_eigen_vectors_train)
+                                                            self._lda_eigen_vectors_train[:m_lda])
 
     def feed(self, features_test):
-        pca_projections_test = get_eigen_projections(features_test, self._mean_face, self._m_pca, self._eigen_vectors)
+        pca_projections_test = get_eigen_projections(features_test, self._mean_face,
+                                                     self._eigen_vectors[:self._m_pca])
         lda_projections_test = get_eigen_projections(pca_projections_test, self._pca_projections_mean_train,
-                                                     self._m_lda,
-                                                     self._lda_eigen_vectors_train)
+                                                     self._lda_eigen_vectors_train[:self._m_lda])
         return self._lda_projections_train, lda_projections_test
